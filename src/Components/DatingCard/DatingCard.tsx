d6fa3dbf9217aef26d734f42431b42cc3f56ca69
marvinxkiyingi <marvin96k@gmail.com>
2022-01-15 15:39:18 +0100

    works partially

--- src/Components/DatingCard/DatingCard.tsx
+++ src/Components/DatingCard/DatingCard.tsx
@@ -9,8 +9,16 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import HeightIcon from '@mui/icons-material/Height';
+import { useDating } from '../../Utils/Contexs/DatingContex';
+import { Button } from '@mui/material';
 
 export const DatingCard = () => {
+  const { dog, getDogs, generateRandomAge, generateRandomHeight, isError } = useDating();
+
+  console.log(dog);
+  // console.log(randomAge);
+  // console.log(randomHeight);
+
   return (
     <Box
       sx={{
@@ -21,47 +29,54 @@ export const DatingCard = () => {
         justifyContent: 'space-evenly',
       }}
     >
-      <Card sx={{ maxWidth: '85%' }}>
-        <CardMedia component='img' height='100%' image='https://cdn2.thedogapi.com/images/Tu1CbaVud.jpg' alt='card media' />
-        <CardContent>
-          <Box
-            sx={{
-              display: 'flex',
-              justifyContent: 'space-between',
-              alignItems: 'center',
-              marginBottom: '0.35em',
-            }}
-          >
-            <Typography gutterBottom variant='h5' component='div'>
-              Lizard
-            </Typography>
+      {isError ? (
+        <Box>
+          <p> something went wrong try again</p>
+          <Button onClick={() => getDogs()}> Try again </Button>
+        </Box>
+      ) : (
+        <Card sx={{ maxWidth: '85%' }}>
+          <CardMedia component='img' height='100%' image={`${dog[0].url}`} alt='card media' />
+          <CardContent>
             <Box
               sx={{
                 display: 'flex',
+                justifyContent: 'space-between',
                 alignItems: 'center',
                 marginBottom: '0.35em',
-                lineHeight: '1.334',
               }}
             >
-              <HeightIcon />
-              50cm
+              <Typography gutterBottom variant='h5' component='div'>
+                {dog[0].breeds[0].name},{`${generateRandomAge()}`}
+              </Typography>
+              <Box
+                sx={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  marginBottom: '0.35em',
+                  lineHeight: '1.334',
+                }}
+              >
+                <HeightIcon />
+                {`${generateRandomHeight()}cm`}
+              </Box>
             </Box>
-          </Box>
 
-          <Typography variant='body2' color='text.secondary'>
-            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica
-          </Typography>
-        </CardContent>
-      </Card>
-
+            <Typography variant='body2' color='text.secondary'>
+              {dog[0].breeds[0].temperament}
+            </Typography>
+          </CardContent>
+        </Card>
+      )}
       <Box
         sx={{
           width: '100%',
           display: 'flex',
           justifyContent: 'space-evenly',
+          marginTop: '0.5rem',
         }}
       >
-        <StyledDeclinedButton sx={{ color: 'white', background: '#092C4C' }}>
+        <StyledDeclinedButton onClick={() => getDogs()} sx={{ color: 'white', background: '#092C4C' }}>
           <CloseIcon />
         </StyledDeclinedButton>
         <StyledLikeButton sx={{ color: 'white', background: '#4C6B84' }}>
--- src/Models/IDatingContex.ts
+++ src/Models/IDatingContex.ts
@@ -1,1 +1,9 @@
-export interface IDatingContext {}
+import { IDog } from './IDog';
+
+export interface IDatingContext {
+  getDogs: () => Promise<void>;
+  dog: [IDog];
+  generateRandomAge: () => void;
+  generateRandomHeight: () => void;
+  isError: boolean;
+}
