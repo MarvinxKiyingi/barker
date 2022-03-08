a3d14b63116a607752f03ee35a3c51d71768c8b9
marvinxkiyingi <marvin96k@gmail.com>
2022-03-08 18:01:07 +0100

    refactor(theme): typography

--- src/Styles/theme.ts
+++ src/Styles/theme.ts
@@ -26,43 +26,56 @@ export const theme = createTheme({
   typography: {
     fontFamily: 'Inter',
     h1: {
-      fontSize: 96,
+      fontSize: 93,
+      letterSpacing: '-1.5px',
     },
     h2: {
-      fontSize: 60,
+      fontSize: 58,
+      letterSpacing: '-0.5px',
     },
     h3: {
-      fontSize: 48,
+      fontSize: 47,
+      letterSpacing: 0,
     },
     h4: {
-      fontSize: 34,
+      fontSize: 33,
+      letterSpacing: '0.25px',
     },
     h5: {
-      fontSize: 24,
+      fontSize: 23,
+      letterSpacing: 0,
     },
     h6: {
-      fontSize: 20,
+      fontSize: 19,
+      letterSpacing: '0.15px',
     },
     subtitle1: {
       fontSize: 16,
+      letterSpacing: '0.15px',
     },
     subtitle2: {
       fontSize: 14,
+      letterSpacing: '0.1px',
     },
     body1: {
       fontSize: 16,
+      letterSpacing: '0.5px',
     },
     body2: {
       fontSize: 14,
+      letterSpacing: '0.25px',
     },
     button: {
       fontSize: 14,
+      letterSpacing: '1.25px',
     },
     caption: {
       fontSize: 12,
+      letterSpacing: '0.4px',
     },
     overline: {
       fontSize: 10,
+      letterSpacing: '1.5px',
     },
   },
 });
